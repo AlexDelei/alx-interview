875d18cb7f98bd9ec77c8e6f300f7c93c432116a
AlexDelei <alexdelei125@gmail.com>
2024-06-10 05:22:50 +0300

    python lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+LockBoxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    determining if all boxes can be opened
+    """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+    return True
