a41a426547f30376f0d88bb06826d7d761e05d45
AlexDelei <alexdelei125@gmail.com>
2024-06-16 22:57:39 +0300

    Calculating the minimum number of operations to be done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,8 +14,8 @@ def minOperations(n: int) -> int:
     dp = [float('inf')] * (n + 1)
     dp[1] = 0
 
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
+    for i in range(2, n + 1):
+        for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + (i // j))
 
