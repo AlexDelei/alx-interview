8abb8be7682a36806935fd9e0c1a95a7fc392d1c
AlexDelei <alexdelei125@gmail.com>
2024-06-20 13:50:20 +0300

    Structuring input log data

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,35 +10,23 @@ from typing import Any
 from time import sleep
 
 
-def log_parsing() -> Any:
-    """
-    Structuring the input data
-    """
-    s = []
-    fmt = r'^(\d{1,3}\.){3}\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] "GET /projects/260 HTTP/1\.1" \d{3} \d+$'
-    for idx, line in enumerate(sys.stdin):
-        try:
-            lst = []
-            inp_format = re.match(fmt, line)
-            if not inp_format:
-                break
-            for i in line.rsplit():
-                lst.append(i)
+s = []
+fmt = r'^(\d{1,3}\.){3}\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] "GET /projects/260 HTTP/1\.1" \d{3} \d+$'
+for idx, line in enumerate(sys.stdin):
+    try:
+        lst = []
+        inp_format = re.match(fmt, line)
+        if not inp_format:
+            break
+        for i in line.rsplit():
+            lst.append(i)
 
-            s.append(int(lst[8]))
-            if idx % 10 == 0:
-                print(f'File size: {sum(s)}')
+        s.append(int(lst[8]))
+        if idx % 10 == 0:
+            print(f'File size: {sum(s)}')
 
-            print(f'{lst[7]}: {random.randint(1, 10)}')
-        except KeyboardInterrupt:
-            continue
+        print(f'{lst[7]}: {random.randint(1, 10)}')
+    except KeyboardInterrupt:
+        continue
 
-    print("Done")
-
-
-if __name__ == '__main__':
-    """
-    Will define the format of the input
-    """
-    log_parsing()
-    signal.signal(signal.SIGINT, log_parsing)
+print("Done")
