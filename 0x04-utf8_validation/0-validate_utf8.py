9ea4751d9d56665d6d46a25536347fbf2dcc36f5
AlexDelei <alexdelei125@gmail.com>
2024-06-27 16:16:41 +0300

    Unicode Transformation Format, the most popular encoding system

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,12 +8,12 @@ validation
 def validUTF8(data):
     """
     UTF-8 is the most popular encoding system and it's
-    work is to translate any unicode character to a 
+    work is to translate any unicode character to a
     matching unique binary string and can also translate
     the binary string back to a unicode character.
     """
     try:
-        byte_data = bytes(data) # converting all data into binary strings
+        byte_data = bytes(data)  # converting all data into binary strings
         # so that the encoding system can work on them
         byte_data.decode('utf-8')
     except (UnicodeDecodeError, ValueError):
