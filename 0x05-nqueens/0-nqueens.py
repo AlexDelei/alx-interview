982da5476f76b5b2443fd86b9ffab03b84124b97
AlexDelei <alexdelei125@gmail.com>
2024-07-11 12:35:16 +0300

    Solving the Nqueens puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,69 +1,50 @@
+
 #!/usr/bin/python3
-"""
-The N queens puzzle
-"""
+"""The N queen Puzzle Module"""
 import sys
+from typing import List
 
 
-def is_safe(board: list[int], row: int, col: int) -> bool:
-    """
-    Check if it's safe to place a queen at board[row][col]
-    This function is called when "col" queens are already
-    placed in columns from 0 to col - 1. We just need
-    to check the left side for attacking queens.
-    """
-    for i in range(col):
-        if board[i] == row or \
-           board[i] - i == row - col or \
-           board[i] + i == row + col:
+def is_safe(board: List[List[int]], row: int, col: int) -> bool:
+    """Check for existence of queen"""
+    for i, j in board:
+        if j == col or \
+           i - j == row - col or \
+           i + j == row + col:
             return False
     return True
 
 
-def solve_nqueens_recursive(
-        board: list[int], col: int, n: int
-        ) -> list[list[int]]:
-    """
-    Use backtracking to find all solutions
-    """
-    if col == n:
-        # All queens are placed successfully
-        return [[[i, board[i]] for i in range(n)]]
+def solve_n_queens(board: List[List[int]], row: int, n: int) -> None:
+    """Solves puzzle"""
+    if row == n:
+        print(board)
+        return
 
-    solutions = []
-    for i in range(n):
-        if is_safe(board, i, col):
-            board[col] = i
-            for solution in solve_nqueens_recursive(board, col + 1, n):
-                solutions.append(solution)
-    return solutions
+    for col in range(n):
+        if is_safe(board, row, col):
+            board.append([row, col])
+            solve_n_queens(board, row + 1, n)
+            board.pop()
 
 
-def solve_nqueens(n: int) -> list[list[int]]:
-    """
-    Solving the Nqueens puzzle challenge of
-    placing N non-attacking queens on an N x N
-    puzzle
-    """
-    board = [-1] * n
-    return solve_nqueens_recursive(board, 0, n)
-
-
-if __name__ == '__main__':
+def main() -> None:
+    """The main function"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1].rstrip("'"))
-    except ValueError:
+        exit(1)
+    if sys.argv[1].isnumeric():
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            exit(1)
+        else:
+            board = []
+            solve_n_queens(board, 0, n)
+    else:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    solutions = solve_nqueens(n)
-    for solution in solutions:
-        print(solution)
+if __name__ == "__main__":
+    main()
